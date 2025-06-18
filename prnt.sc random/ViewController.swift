018deb9ef369620fb10bc9a53ca11c0cbeb8c423
Banan_1984 <niketozzz@gmail.com>
2025-06-18 16:23:55 +0300

    Initial Commit

---
+++ prnt.sc random/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prnt.sc random
+//
+//  Created by Никита Сорочинский on 6/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
